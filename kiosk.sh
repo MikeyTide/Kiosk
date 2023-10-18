960bbf3cf1e96e96ff4d33ed0405465b8dc9fa6d
mikey Tide <gabidullin.aleks@yandex.ru>
2023-10-18 11:07:45 +0400

    init

--- kiosk.sh
+++ kiosk.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 site=url/site.ru
 # Запустить Firefox на странице сseu.ru в полноэкранном режиме
-firefox $site -kiosk & > /dev/null 2>&1
+firefox "$site" -kiosk & > /dev/null 2>&1
 
 # Бесконечный цикл для проверки активности пользователя
 while true; do
   idle_time=xprintidle # Получить время бездействия пользователя в миллисекундах
   if [ $idle_time -gt 10000 ]; then # Если бездействие пользователя превышает 1 минуту
-    firefox www.sseu.ru -kiosk  & > /dev/null 2>&1  # Обновить страницу в Firefox
+    firefox "$site" -kiosk  & > /dev/null 2>&1  # Обновить страницу в Firefox
     sleep 5  # Ждать 5 секунд перед проверкой снова
   fi
   sleep 1  # Ждать 1 секунду перед проверкой снова
